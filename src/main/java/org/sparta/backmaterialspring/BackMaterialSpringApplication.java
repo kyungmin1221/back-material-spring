c71ec4695573bb2bb75d0a5ab84d9719edd28358
Robbie <camp.robbie.choi@gmail.com>
2024-03-13 21:25:00 +0900

    init commit

---
+++ src/main/java/org/sparta/backmaterialspring/BackMaterialSpringApplication.java
@@ -0,0 +1,13 @@
+package org.sparta.backmaterialspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackMaterialSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackMaterialSpringApplication.class, args);
+    }
+
+}
