c235b68d366c59bbc4d60bc59d5721500f4069f8
Robbie <camp.robbie.choi@gmail.com>
2024-03-15 22:31:16 +0900

    feat: BaseEntity 추가

---
+++ src/main/java/org/sparta/backmaterialspring/common/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package org.sparta.backmaterialspring.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+class JpaConfig {
+}
--- src/main/java/org/sparta/backmaterialspring/common/entity/BaseEntity.java
+++ src/main/java/org/sparta/backmaterialspring/common/entity/BaseEntity.java
@@ -1,4 +1,21 @@
 package org.sparta.backmaterialspring.common.entity;
 
-public class BaseEntity {
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
 }
