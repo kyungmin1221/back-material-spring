c4f15411100838d58c639304dc2c29c3607ca189
smallzooDev <joonkyu_kang@Joonkyu-Kangui-MacBookPro.local>
2024-03-17 17:34:30 +0900

    chore: add comment on service

--- src/main/java/org/sparta/backmaterialspring/payment/service/PointService.java
+++ src/main/java/org/sparta/backmaterialspring/payment/service/PointService.java
@@ -3,5 +3,11 @@ package org.sparta.backmaterialspring.payment.service;
 import org.sparta.backmaterialspring.payment.entity.Point;
 
 public interface PointService {
+    /**
+     * 포인트를 사용처리한다
+     * @param point point entity
+     * @param amountToUse 사용할 금액
+     * @param reason 사용 사유
+     */
     void usePoint(Point point, int amountToUse, String reason);
 }
